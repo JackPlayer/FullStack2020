eac831ca807ee04b250acac3ff60e4510520e310
Jack Player <jackson.m.player@gmail.com>
2020-09-12 16:12:29 -0700

    2.8: Added number functionality

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Jack Player' }
+    { name: 'Jack Player', number: "250-555-5555"}
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
-  const handleAddName = (e) => {
+  const handleAddEntry = (e) => {
     e.preventDefault();
 
     let alreadyExists = false
@@ -18,23 +19,34 @@ const App = () => {
       }
     })
 
-    if (alreadyExists) { 
-      setNewName('')  
+    if (newName === '' || newNumber === '') {
+      window.alert('One or both of the fields are blank')
+    }
+
+    if (alreadyExists || newName === '' || newNumber === '') { 
+      setNewName('')
+      setNewNumber('')  
       return 
     }
+
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     setPersons(persons.concat(newPerson))
+    setNewNumber('')
     setNewName('')
   }
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleAddName}>
+      <form onSubmit={handleAddEntry}>
         <div>
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -43,7 +55,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => {
-          return <li key={person.name}>{person.name}</li>
+          return <li key={person.name}>{person.name}   {person.number}</li>
         })}
       </ul>
       
