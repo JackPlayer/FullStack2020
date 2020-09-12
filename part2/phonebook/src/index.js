fb60322ccf0695f14115e6cacd1403fd8e75da90
Jack Player <jackson.m.player@gmail.com>
2020-09-12 15:51:39 -0700

    2.6 Phonebook app started

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Jack Player' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  const handleAddName = (e) => {
+    e.preventDefault();
+    const newPerson = {
+      name: newName
+    }
+
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddName}>
+        <div>
+          name: <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => {
+          return <li key={person.name}>{person.name}</li>
+        })}
+      </ul>
+      
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+export default App
