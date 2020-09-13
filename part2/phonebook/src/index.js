7dd1df56d43cde6a973b32c16d8c4baf6a784dd9
Jack Player <jackson.m.player@gmail.com>
2020-09-12 17:46:10 -0700

    2.11: Implemented server fetching of persons

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,17 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
+
+import axios from 'axios'
 
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Jack Player', number: "250-555-5555"}
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter]  = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response) => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleAddEntry = (e) => {
     e.preventDefault();
@@ -43,6 +51,7 @@ const App = () => {
     setNewNumber('')
     setNewName('')
   }
+
   return (
     <div>
       <h1>Phonebook</h1>
