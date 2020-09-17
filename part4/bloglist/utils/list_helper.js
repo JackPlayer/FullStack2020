8ab0a3bd71c8f0e147134b5e8f45fb0cdfc12a2b
Jack Player <jackson.m.player@gmail.com>
2020-09-17 14:47:39 -0700

    4.6-4.7: Added mostLikes and mostBlogs helper functions and tests

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -23,7 +23,70 @@ const favoriteBlog = (blogs) => {
   return favorite
 }
 
+const mostBlogs = (blogs) => {
+  const numberOfBlogsList = []
+
+  blogs.forEach((blog) => {
+    const isAdded = numberOfBlogsList.filter((elem) => blog.author === elem.author).length > 0
+    if (isAdded) {
+      numberOfBlogsList.forEach((elem) => {
+        if (elem.author === blog.author) {
+          elem.blogs += 1
+        }
+      })
+    } else {
+      const newElem = {
+        author: blog.author,
+        blogs: 1,
+      }
+      numberOfBlogsList.push(newElem)
+    }
+  })
+  let topBlogger = {}
+  numberOfBlogsList.forEach((blogger) => {
+    if (Object.keys(topBlogger).length === 0 || blogger.blogs > topBlogger.blogs) {
+      topBlogger = {
+        ...blogger,
+      }
+    }
+  })
+  return topBlogger
+}
+
+const mostLikes = (blogs) => {
+  const numberOfLikesList = []
+
+  blogs.forEach((blog) => {
+    const isAdded = numberOfLikesList.filter((elem) => blog.author === elem.author).length > 0
+    if (isAdded) {
+      numberOfLikesList.forEach((elem) => {
+        if (elem.author === blog.author) {
+          elem.likes += blog.likes
+        }
+      })
+    } else {
+      const newElem = {
+        author: blog.author,
+        likes: blog.likes,
+      }
+      numberOfLikesList.push(newElem)
+    }
+  })
+
+  let topBlogger = {}
+  numberOfLikesList.forEach((blogger) => {
+    if (Object.keys(topBlogger).length === 0 || blogger.likes > topBlogger.likes) {
+      topBlogger = {
+        ...blogger,
+      }
+    }
+  })
+  return topBlogger
+}
+
 module.exports = {
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 }
