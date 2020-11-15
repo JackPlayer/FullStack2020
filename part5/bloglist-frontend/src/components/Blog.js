ee9e6b5a983bb4b5405506688487cb2ac17ec905
Jack Player <jackson.m.player@gmail.com>
2020-11-15 15:27:27 -0800

    5.7: Improved styling and added like button

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,24 @@
 import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
-
+import Toggleable from './Toggleable'
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    padding: '1rem',
+    width: '20%',
+    marginTop: '20px',
+    backgroundColor: '#EEE',
+    color: '#333',
+  }
+  return (
+    <div style={blogStyle}>
+      <h2 style={{textTransform: 'uppercase', textAlign: 'center'}}>{blog.title}</h2>
+      <p style={{"font-style": "italic"}}>Author: {blog.author}</p>
+      <Toggleable buttonPrompt="View">
+        <p>URL: {blog.url}</p>
+        <p>Likes: {blog.likes}</p> <button style={{display: 'inline-block'}}>Like</button>
+      </Toggleable>
+    </div>
+  )
+   
+}
+  
 export default Blog
