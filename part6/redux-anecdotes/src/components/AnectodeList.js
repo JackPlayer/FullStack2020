1bb3823fcb31adc7955bc266885a04fa177c550f
Jack Player <jackson.m.player@gmail.com>
2020-11-18 15:41:23 -0800

    6.8: Refactored anecdotes into Anecdote and AnecdoteList component

---
+++ part6/redux-anecdotes/src/components/AnectodeList.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { voteFor } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({content, votes, id}) => {
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch(voteFor(id))
+  }
+
+  return (
+    <div>
+      <div>
+        {content}
+      </div>
+      <div>
+        has {votes}
+        <button onClick={() => vote(id)}>vote</button>
+      </div>
+    </div>
+  )
+}
+const AnecdoteList = () => {
+  const anecdotes = useSelector(state => state)
+
+  const sortedByVotesAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
+
+  const anecdotesRender = sortedByVotesAnecdotes.map((anecdote) => <Anecdote key={anecdote.id} content={anecdote.content} id={anecdote.id} votes={anecdote.votes} />)
+  
+  return (
+    <div id="anecdote-list">
+      {anecdotesRender}
+    </div>
+  )
+}
+
+export default AnecdoteList
