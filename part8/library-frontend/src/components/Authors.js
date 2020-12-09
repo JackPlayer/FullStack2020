0f5b407fcec3698d9092d037248debf76f2c4f9e
Jack Player <jackson.m.player@gmail.com>
2020-12-09 14:03:55 -0800

    8.8: Implemented authors view

---
+++ part8/library-frontend/src/components/Authors.js
@@ -0,0 +1,51 @@
+  
+import React from 'react'
+import { ALL_AUTHORS } from '../queries/queries'
+import { useQuery } from '@apollo/client'
+
+const Authors = (props) => {
+  const authorsResult = useQuery(ALL_AUTHORS)
+
+  if (!props.show) {
+    return null
+  }
+
+
+  const renderAuthorsTable = () => {
+    if (authorsResult.loading) {
+      return (
+        <p>Loading...</p>
+      )
+    }
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authorsResult.data.allAuthors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    )
+  }
+  return (
+    <div>
+      <h2>authors</h2>
+      { renderAuthorsTable() }
+    </div>
+  )
+}
+
+export default Authors
