4f7205bd7a552e9855fc2c05909c34017da48ee2
Jack Player <jackson.m.player@gmail.com>
2021-01-05 13:58:49 -0800

    8.19: Added filter lists

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,22 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries/queries'
 
 
 
 const Books = (props) => {
+  const [genre, setGenre] = useState('all')
   const booksResult = useQuery(ALL_BOOKS)
   if (!props.show) {
     return null
   }
 
+  const renderSelector = () => {
+    if(!booksResult.loading) {
+      const books = booksResult.data.allBooks;
+      const select = ['all']
+      
+      books.forEach((book) => {
+        const genres = book.genres;
+
+        genres.forEach((genre) =>{
+          if (!select.includes(genre)) select.push(genre)
+        })
+      })
+
+      const selectRender = select.map((selectItem) => {
+        return (
+          <option key={selectItem} value={selectItem}>{selectItem}</option>
+        )
+      })
+      return (
+        <select onChange={(e) => setGenre(e.target.value)}>
+          {selectRender}
+        </select>
+      )
+    }
+    return (
+      <select><option>Loading...</option></select>
+    )
+    
+  }
   const renderBooksTable = () => {
     if (booksResult.loading) {
       return (
         <p>Loading...</p>
       )
     }
-
+    const books = booksResult.data.allBooks;
+    const booksToRender = (genre === 'all') ? books : books.filter((book) => book.genres.includes(genre))
     return (
       <table>
         <tbody>
@@ -29,7 +60,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {booksResult.data && booksResult.data.allBooks.map(a =>
+          {booksToRender && booksToRender.map(a =>
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -45,6 +76,7 @@ const Books = (props) => {
   return (
     <div>
         <h2>books</h2>
+        { renderSelector() }
         { renderBooksTable() }
     </div>
   )
