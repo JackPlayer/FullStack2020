62e1c44f9e591bf7b5438f01881361e415f0e792
Jack <jackson.m.player@gmail.com>
2021-01-16 13:18:30 -0800

    Initialized patientor-backend project

---
+++ part9/patientor-backend/index.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get('/ping', (_req, res) => {
+    console.log("Someone is on ping");
+    res.send('pong')
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
+})
