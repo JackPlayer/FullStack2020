59b0f187556299d3e0ee6f98b567657be1765b4a
Jack <jackson.m.player@gmail.com>
2021-01-16 14:57:59 -0800

    9.11: Frontend works with backend

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,14 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import diagnosisRouter from './routes/diagnosisRouter';
 import patientsRouter from './routes/patientsRouter';
 
 const app = express();
 
 app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 
 const PORT = 3500;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
     console.log("Someone is on ping");
     res.send('pong');
 });
